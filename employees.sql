93327c257926a3d06b853c1814182d194095b3c1
JAld3r <jeffjalder@gmail.com>
2023-05-27 14:18:30 -0700

    sql-challenge

--- employees.sql
+++ employees.sql
@@ -2,66 +2,87 @@
 -- Link to schema: https://app.quickdatabasediagrams.com/#/d/oJ5mg3
 -- NOTE! If you have used non-SQL datatypes in your design, you will have to change these here.
 
-
-CREATE TABLE `departments` (
-    `dept_no` VARCHAR  NOT NULL ,
-    `dept_name` VARCHAR  NOT NULL ,
+-- create tables for each CSV file to import data into
+CREATE TABLE departments (
+    dept_no VARCHAR  NOT NULL ,
+    dept_name VARCHAR  NOT NULL ,
     PRIMARY KEY (
-        `dept_no`
+        dept_no
     )
 );
 
-CREATE TABLE `titles` (
-    `title_id` VARCHAR  NOT NULL ,
-    `title` VARCHAR  NOT NULL ,
+CREATE TABLE titles (
+    title_id VARCHAR  NOT NULL ,
+    title VARCHAR  NOT NULL ,
     PRIMARY KEY (
-        `title_id`
+        title_id
     )
 );
 
-CREATE TABLE `employees` (
-    `emp_no` INT  NOT NULL ,
-    `emp_title_id` VARCHAR  NOT NULL ,
-    `birth_date` DATE  NOT NULL ,
-    `first_name` VARCHAR  NOT NULL ,
-    `last_name` VARCHAR  NOT NULL ,
-    `sex` VARCHAR  NOT NULL ,
-    `hire_date` DATE  NOT NULL ,
+CREATE TABLE employees (
+    emp_no INT  NOT NULL ,
+    emp_title_id VARCHAR  NOT NULL ,
+    birth_date DATE  NOT NULL ,
+    first_name VARCHAR  NOT NULL ,
+    last_name VARCHAR  NOT NULL ,
+    sex VARCHAR  NOT NULL ,
+    hire_date DATE  NOT NULL ,
     PRIMARY KEY (
-        `emp_no`
+        emp_no
     )
 );
 
-CREATE TABLE `dept_emp` (
-    `emp_no` INT  NOT NULL ,
-    `dept_no` VARCHAR  NOT NULL 
+CREATE TABLE dept_emp (
+    emp_no INT  NOT NULL ,
+    dept_no VARCHAR  NOT NULL 
 );
 
-CREATE TABLE `dept_manager` (
-    `dept_no` VARCHAR  NOT NULL ,
-    `emp_no` INT  NOT NULL 
+CREATE TABLE dept_manager (
+    dept_no VARCHAR  NOT NULL ,
+    emp_no INT  NOT NULL 
 );
 
-CREATE TABLE `salaries` (
-    `emp_no` INT  NOT NULL ,
-    `salary` INT  NOT NULL 
+CREATE TABLE salaries (
+    emp_no INT  NOT NULL ,
+    salary INT  NOT NULL 
 );
 
-ALTER TABLE `employees` ADD CONSTRAINT `fk_employees_emp_title_id` FOREIGN KEY(`emp_title_id`)
-REFERENCES `titles` (`title_id`);
+ALTER TABLE employees ADD CONSTRAINT fk_employees_emp_title_id FOREIGN KEY(emp_title_id)
+REFERENCES titles (title_id);
 
-ALTER TABLE `dept_emp` ADD CONSTRAINT `fk_dept_emp_emp_no` FOREIGN KEY(`emp_no`)
-REFERENCES `employees` (`emp_no`);
+ALTER TABLE dept_emp ADD CONSTRAINT fk_dept_emp_emp_no FOREIGN KEY(emp_no)
+REFERENCES employees (emp_no);
 
-ALTER TABLE `dept_emp` ADD CONSTRAINT `fk_dept_emp_dept_no` FOREIGN KEY(`dept_no`)
-REFERENCES `departments` (`dept_no`);
+ALTER TABLE dept_emp ADD CONSTRAINT fk_dept_emp_dept_no FOREIGN KEY(dept_no)
+REFERENCES departments (dept_no);
 
-ALTER TABLE `dept_manager` ADD CONSTRAINT `fk_dept_manager_dept_no` FOREIGN KEY(`dept_no`)
-REFERENCES `departments` (`dept_no`);
+ALTER TABLE dept_manager ADD CONSTRAINT fk_dept_manager_dept_no FOREIGN KEY(dept_no)
+REFERENCES departments (dept_no);
 
-ALTER TABLE `dept_manager` ADD CONSTRAINT `fk_dept_manager_emp_no` FOREIGN KEY(`emp_no`)
-REFERENCES `employees` (`emp_no`);
+ALTER TABLE dept_manager ADD CONSTRAINT fk_dept_manager_emp_no FOREIGN KEY(emp_no)
+REFERENCES employees (emp_no);
 
-ALTER TABLE `salaries` ADD CONSTRAINT `fk_salaries_emp_no` FOREIGN KEY(`emp_no`)
-REFERENCES `employees` (`emp_no`);
+ALTER TABLE salaries ADD CONSTRAINT fk_salaries_emp_no FOREIGN KEY(emp_no)
+REFERENCES employees (emp_no);
+
+
+-- List the employee number, last name, first name, sex, and salary of each employee.
+
+
+
+
+-- List the first name, last name, and hire date for the employees who were hired in 1986.
+
+-- List the manager of each department along with their department number, department name, employee number, last name, and first name.
+
+-- List the department number for each employee along with that employee’s employee number, last name, first name, and department name.
+
+-- List first name, last name, and sex of each employee whose first name is Hercules and whose last name begins with the letter B.
+
+-- List each employee in the Sales department, including their employee number, last name, and first name.
+
+-- List each employee in the Sales and Development departments, including their employee number, last name, first name, and department name.
+
+-- List the frequency counts, in descending order, of all the employee last names (that is, how many employees share each last name).
+
 
