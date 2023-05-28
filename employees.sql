74997725f11601296368e665b0ff2f62f4868b7c
JAld3r <jeffjalder@gmail.com>
2023-05-28 15:31:05 -0700

    sql-challenge

--- employees.sql
+++ employees.sql
@@ -75,17 +75,39 @@ e.emp_no=s.emp_no;
 -- List the first name, last name, and hire date for the employees who were hired in 1986.
 SELECT first_name, last_name, hire_date
 FROM employees
-WHERE hire_date BETWEEN '1/1/1986' AND '12/31/1986'
+WHERE hire_date BETWEEN '1/1/1986' AND '12/31/1986';
 
 -- List the manager of each department along with their department number, department name, employee number, last name, and first name.
+SELECT dm.dept_no, dep.dept_name, dm.emp_no, e.last_name, e.first_name
+FROM dept_manager AS dm
+INNER JOIN departments AS dep ON dm.dept_no = dep.dept_no
+INNER JOIN employees AS e ON dm.emp_no = e.emp_no;
 
 -- List the department number for each employee along with that employee’s employee number, last name, first name, and department name.
+SELECT de.dept_no, e.emp_no, e.last_name, e.first_name, dep.dept_name
+FROM dept_emp AS de
+INNER JOIN employees AS e ON de.emp_no = e.emp_no
+INNER JOIN departments AS dep ON de.dept_no = dep.dept_no;
 
 -- List first name, last name, and sex of each employee whose first name is Hercules and whose last name begins with the letter B.
+SELECT first_name, last_name, sex
+FROM employees
+WHERE first_name = 'Hercules'
+AND last_name LIKE 'B%';
 
 -- List each employee in the Sales department, including their employee number, last name, and first name.
+-- determine which dept number is sales (d007)
+SELECT * FROM departments
+WHERE dept_name = 'Sales';
+
+SELECT de.emp_no, e.last_name, e.first_name
+FROM dept_emp AS de
+INNER JOIN employees AS e ON de.emp_no = e.emp_no
+WHERE dept_no = 'd007';
 
 -- List each employee in the Sales and Development departments, including their employee number, last name, first name, and department name.
+
+
 
 -- List the frequency counts, in descending order, of all the employee last names (that is, how many employees share each last name).
 
