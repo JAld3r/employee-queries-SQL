75da1d74a14f70cacd93c9fe9f9957f1b1b0c762
JAld3r <jeffjalder@gmail.com>
2023-05-28 14:25:40 -0700

    sql-challenge

--- employees.sql
+++ employees.sql
@@ -67,11 +67,15 @@ REFERENCES employees (emp_no);
 
 
 -- List the employee number, last name, first name, sex, and salary of each employee.
-
-
-
+SELECT e.emp_no, e.last_name, e.first_name, e.sex, s.salary
+FROM employees as e
+INNER JOIN salaries as s ON
+e.emp_no=s.emp_no;
 
 -- List the first name, last name, and hire date for the employees who were hired in 1986.
+SELECT first_name, last_name, hire_date
+FROM employees
+WHERE hire_date BETWEEN '1/1/1986' AND '12/31/1986'
 
 -- List the manager of each department along with their department number, department name, employee number, last name, and first name.
 
